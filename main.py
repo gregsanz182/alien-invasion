28f9852252ddf93a9ae748b58e481c4f38432f63
Gregory Sánchez <sanchz93@gmail.com>
2018-09-18 20:10:00 -0400

    Finished aim and layers calculation.

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from ship import Ship
 
 if __name__ == "__main__":
     with open("example.txt", "r") as input_file:
-        n_ships = input_file.readline()
-        for i in range(int(n_ships)):
+        n_ships = int(input_file.readline())
+        for i in range(n_ships):
             line = input_file.readline()
             ship_info = line.split(" ")
             ship = Ship(int(ship_info[0]), int(ship_info[1]), float(ship_info[2]))
@@ -13,4 +13,8 @@ if __name__ == "__main__":
 
             layer = ship.check_layer()
             while(layer):
+                print(";".join([str(cmd) for cmd in layer]), end="")
                 layer = ship.check_layer()
+                if layer: print(" ", end="")
+            
+            if i < n_ships+1: print("")
--- ship.py
+++ ship.py
@@ -3,13 +3,14 @@ import re
 
 class Command():
 
-    def __init__(self, row, col, scale):
+    def __init__(self, row, col, scale, name):
         self.min_x = row
         self.min_y = col
         self.max_x = row
         self.max_y = col
         self.area = scale*scale
-        self.shoot_pos = set()
+        self.shoot_pos = 0, 0
+        self.name = name
 
     def add_corner(self, row, col):
         if(row < self.min_x):
@@ -21,6 +22,16 @@ class Command():
             self.min_y = col
         elif(col > self.max_y):
             self.max_y = col
+    
+    def calc_area_and_aim(self, scale):
+        self.area = ((self.max_x - self.min_x) * (self.max_y - self.min_y) * scale)
+        self.shoot_pos = (
+            ((self.min_x + self.max_x + 1) * scale)/2,
+                        ((self.min_y + self.max_y + 1)*scale)/2
+        )
+    
+    def __repr__(self):
+        return "{0}:{1:.3f},{2:.3f}".format(self.name, self.shoot_pos[0], self.shoot_pos[1])
 
 class Ship():
 
@@ -43,18 +54,26 @@ class Ship():
     def __add_command(self, row, col, value):
         command = self.commands.get(value)
         if(command is None):
-            self.commands[value] = Command(row, col, self.scale)
+            self.commands[value] = Command(row, col, self.scale, value)
         else:
             command.add_corner(row, col)
+            command.calc_area_and_aim(self.scale)
 
     def check_layer(self):
-        layers_commands = dict()
+        layers_commands = []
         commands_aux = self.commands.copy()
         for command_name, command in commands_aux.items():
             if self.__check_command(command_name, command):
-                layers_commands[command_name] = command
+                layers_commands.append(command)
                 del self.commands[command_name]
+
+        #sorting by area
+        layers_commands = sorted(layers_commands, key=lambda layer: layer.area)
+        #sorting by name
+        layers_commands = sorted(layers_commands, key=lambda layer: layer.area)
+
         self.__remove_layer(layers_commands)
+        
         return layers_commands
 
     def __check_command(self, command_name: str, command: Command):
@@ -65,7 +84,7 @@ class Ship():
         return True
 
     def __remove_layer(self, layers_commands):
-        for layer, command in layers_commands.items():
+        for command in layers_commands:
             for i in range(command.min_x, command.max_x+1):
                 for j in range(command.min_y, command.max_y+1):
                     if self.regexp.fullmatch(self.surface[i][j]):
